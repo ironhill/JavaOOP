b55e91bda35a473ec941fd6ffd99021f779d2ec0
Ironhill <isamanchuk@gmail.com>
2015-10-08 23:38:38 +0300

    Kassa commit

---
+++ src/week3/day1/homework/trainkassa/TestKassa.java
@@ -0,0 +1,13 @@
+package week3.day1.homework.trainkassa;
+
+/**
+ * Created by Ironhill on 08.10.2015.
+ */
+public class TestKassa {
+    public static void main(String[] args) {
+
+        Train train = new Train(3);
+
+
+    }
+}
---
+++ src/week3/day1/homework/trainkassa/Train.java
@@ -0,0 +1,30 @@
+package week3.day1.homework.trainkassa;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Ironhill on 08.10.2015.
+ */
+public class Train {
+    private int trainNumber = Ticket.trainNumber;
+    private Date dateDispatch = Ticket.dateDispatch;
+    private int carriagesAmount;
+    private ArrayList trainArray ;
+
+    public Train(int carriagesAmount) {
+        this.carriagesAmount = carriagesAmount;
+        this.trainArray = new ArrayList(carriagesAmount);
+    }
+
+    public void addPlats(int amountCarriages, PlatscardCarriage plats){
+        for (int i = 0; i < amountCarriages; i++) {
+            trainArray.add(i, plats);
+        }
+    }
+    public void addCupe(int amountCarriages, CupeCarriage cupe){
+        for (int i = 0; i < amountCarriages; i++) {
+            trainArray.add(i, cupe);
+        }
+    }
+}
