c1f363f9be488549ee4e06c63fcdebc02d295521
Ironhill <isamanchuk@gmail.com>
2015-10-10 13:25:12 +0300

    New changelist

---
+++ src/week3/homework/trainkassa/CupeCarriage.java
@@ -0,0 +1,12 @@
+package week3.homework.trainkassa;
+
+/**
+ * Created by Ironhill on 08.10.2015.
+ */
+public class CupeCarriage extends  Carriage{
+
+    public CupeCarriage(int placesAmount, float price) {
+        super(placesAmount, price);
+        super.setCarrige();
+    }
+}
---
+++ src/week3/homework/trainkassa/LuxCarriage.java
@@ -0,0 +1,14 @@
+package week3.homework.trainkassa;
+
+/**
+ * Created by Ironhill on 08.10.2015.
+ */
+public class LuxCarriage extends Carriage{
+
+
+    public LuxCarriage(int placesAmount, float price) {
+        super(placesAmount, price);
+        super.setCarrige();
+    }
+
+}
